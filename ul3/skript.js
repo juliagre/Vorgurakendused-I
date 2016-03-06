347a6a643859126162827f8281c639580b69a765
juliagre <julia.grets@gmail.com>
2016-03-06 22:07:30 +0200

    Ulesanne 3
    Ulesanne 3

---
+++ ul3/skript.js
@@ -0,0 +1,14 @@
+﻿function timer(){
+	var endDate = new Date("2016-02-21T23:59:59");
+	var time = new Date();
+	var difference = endDate-time;
+	if (endDate>time){
+		var päevad = parseInt(difference/(1000*60*60*24));
+		var tunnid = parseInt(difference/(1000*60*60))%24;
+		var minutid = parseInt(difference/(1000*60))%60;
+		var sekundid = parseInt(difference/(1000))%60;
+		document.getElementById('myText').innerHTML = päevad + " päeva: " + tunnid + " tundi: " + minutid + " minutit: " + sekundid +" sekundit"
+	}
+	else {alert("TÖÖDE ESITAMISE TAHTAEG MÖÖDUNUD!")}
+	
+}
